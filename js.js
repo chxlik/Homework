11644c30a787d18e273ceb8a940f74dec6c2c6a1
chxlik <79369013+chxlik@users.noreply.github.com>
2021-03-26 21:48:57 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,51 @@
+
+
+// let arrNum = [22, 33, 60, 71, 66];
+// let arrString = ['Hello', 'Girl', 'Tomato', 'Grass', 'Water']
+// let diff = [66, 'hi', true, 'water', 13]
+// console.log(arrNum)
+// console.log(arrString)
+// console.log(diff)
+//
+//
+// let home = []
+//     home[0] = 'kitchen';
+//     home[1] = 7;
+//     home[2] = 'bed';
+//     home[3] = 'chill';
+
+
+
+// for (let i = 0; i < 10; i++) {
+//   document.write('<div> My name is Nikita </div>')
+// }
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     document.write('<div> My name' + [i] + 'is Nikita </div>')
+// }
+
+
+
+// let i = 0
+//
+// while (i < 20) {
+//
+//     document.write('<div> <h1> Hello world </h1> </div>')
+//
+//     i++
+// }
+
+
+
+// let i = 0
+//
+//     while (i < 20) {
+//
+//     document.write(('<div> <h1> Hello' + [i] + 'world </h1> </div>'))
+//     i++
+// }
+
+
+
